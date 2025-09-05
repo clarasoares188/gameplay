7cff0bcf799795802d8aa65f5fdae80ccda0313f
clarasoares188 <clarabeatriz2324@gmail.com>
2025-09-05 11:32:46 -0300

    0509

---
+++ src/screens/signIn/index.tsx
@@ -0,0 +1,20 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export function signIn() {
+  return (
+    <View style={styles.container}>
+      <Text>tela de entrada</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#df9be6ff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
